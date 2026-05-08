060aa8ac6a56874755f96bbd7a99dcaecd2898c5
ivankiranchuk-glitch <ivankiranchuk@gmail.com>
2026-05-08 23:21:07 +0300

    Add Gradle project settings

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+rootProject.name = "DirectLink"
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+include(":shared")
+include(":android")
+include(":desktop")
